ceb4e5c7fbad230dcddeccc2370c0843b48ccc13
Ankit Das <www.das5471@gmail.com>
2023-09-23 03:33:49 +0530

    Form Wave Animation

---
+++ Form Wave Animation/script.js
@@ -0,0 +1,8 @@
+const lables = document.querySelectorAll('.form-control label')
+
+lables.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+});
