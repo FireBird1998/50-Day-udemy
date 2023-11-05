f0213628896aac01fe6ea9864a2cd415ff89b6f5
Ankit Das <www.das5471@gmail.com>
2023-11-06 04:09:04 +0530

    stlish check bok and celendely intregration

---
+++ CheckBox Toggle/script.js
@@ -0,0 +1,29 @@
+const toggles = document.querySelectorAll('.toggle');
+const good = document.querySelector('#good');
+const cheap = document.querySelector('#cheap');
+const fast = document.querySelector('#fast');
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('change', (e) => {
+        dotheTrick(e.target);
+    });
+});
+
+function dotheTrick(theClickedOne) {
+    if (good.checked && cheap.checked && fast.checked) {
+        if (good === theClickedOne) {
+            fast.checked = false;
+        }
+
+        if (cheap === theClickedOne) {
+            good.checked = false;
+        }
+
+        if (fast === theClickedOne) {
+            cheap.checked = false;
+        }
+    } else {
+        console.log('error');
+    }
+
+}
